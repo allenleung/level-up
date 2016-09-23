55e16cad5bcba0888b4f0291a6eb3af3b39ae2ac
Allen Leung <allenleung@users.noreply.github.com>
2016-09-23 16:48:13 -0700

    Input.py on my own

---
+++ input.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+# Initialize a variable with a user-specified value
+user = input( 'I am Python. What is your name? :' )
+# Output a string and a variable value
+print( 'Welcome' , user )
