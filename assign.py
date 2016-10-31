4aa057f6b9838b061d9d6308c1231580aa2d1010
Allen Leung <allenleung@users.noreply.github.com>
2016-10-31 15:16:13 -0700

    More Maths

---
+++ assign.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+a = 8
+b = 4
+print('Assign Values:\t\t','a=',a,'/tb=',b)
+
+a+=b
+print('Add & Assign:\t\t','a=',a,'(8+=4)')
+
+a-=b
+print('Subtract & Assign:\t','a=',a,'(12 - 4)'
+
