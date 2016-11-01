cf900cadf569c1a87ffe4f22caa6cfcbdeaaffa1
Allen Leung <allenleung@users.noreply.github.com>
2016-11-01 10:42:19 -0700

    Condition

---
+++ condition.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+a = 1
+b = 2
+
+print('\nVariable a is :','One' if (a == 1) else 'Not One')
+print('Variable a is :','Even' if (a % 2 ==0) else 'Odd')
+
+print('\nVariable b is :' ,'One' if (b == 1) else 'Not One')
+print('Variable b is :','Even' if (b % 2== 0) else 'Odd')
+
+max = a if (a > b) else b
+
+print('\nGreater Value is:',max)
