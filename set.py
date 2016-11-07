3dcef72adea85d66d722570c0ebc23adfa235bc1
Allen Leung <allenleung@users.noreply.github.com>
2016-11-07 13:36:35 -0800

    Set.py

---
+++ set.py
@@ -0,0 +1,15 @@
+#!/usr/local/bin/python3
+zoo = ('Kangaroo','Leopard','Moose')
+print('Tuple:',zoo,'\tLength:',len(zoo))
+print(type(zoo))
+
+bag={'Red','Green','Blue'}
+print('\nSet:',bag,'\tLength',len(bag))
+print(type(bag))
+
+print('\nIs Green In bag Set?:','Green'in bag)
+print('Is Orange In bag Set?:','Orange'in bag
+
+box={'Red','Purple','Yellow'}
+print('\nSet:',box,'\tLength',len(box))
+print('Common To Both Sets:',bag.intersection(box))
